d6482824156ce5fd22a861e5e4364d821604533b
adriennzeman <adrienn.zeman@stud.uni-corvinus.hu>
2022-01-07 17:10:46 +0100

    Adatok megjelenitese diagramon es Dinamikus funkciok

--- MnbCurrencyReader/MnbCurrencyReader/Form1.Designer.cs
+++ MnbCurrencyReader/MnbCurrencyReader/Form1.Designer.cs
@@ -28,30 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dgwRates = new System.Windows.Forms.DataGridView();
+            this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.cbCurrency = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgwRates)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).BeginInit();
             this.SuspendLayout();
             // 
             // dgwRates
             // 
             this.dgwRates.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgwRates.Location = new System.Drawing.Point(0, 2);
+            this.dgwRates.Location = new System.Drawing.Point(0, 52);
             this.dgwRates.Name = "dgwRates";
             this.dgwRates.RowHeadersWidth = 51;
             this.dgwRates.RowTemplate.Height = 24;
-            this.dgwRates.Size = new System.Drawing.Size(479, 437);
+            this.dgwRates.Size = new System.Drawing.Size(479, 401);
             this.dgwRates.TabIndex = 0;
+            // 
+            // chartRateData
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend6);
+            this.chartRateData.Location = new System.Drawing.Point(485, 52);
+            this.chartRateData.Name = "chartRateData";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chartRateData.Series.Add(series6);
+            this.chartRateData.Size = new System.Drawing.Size(516, 401);
+            this.chartRateData.TabIndex = 1;
+            this.chartRateData.Text = "chart1";
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Location = new System.Drawing.Point(0, 13);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(200, 22);
+            this.dtpStart.TabIndex = 2;
+            this.dtpStart.ValueChanged += new System.EventHandler(this.dtpStart_ValueChanged);
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Location = new System.Drawing.Point(222, 12);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(200, 22);
+            this.dtpEnd.TabIndex = 3;
+            this.dtpEnd.ValueChanged += new System.EventHandler(this.dtpStart_ValueChanged);
+            // 
+            // cbCurrency
+            // 
+            this.cbCurrency.FormattingEnabled = true;
+            this.cbCurrency.Items.AddRange(new object[] {
+            "EUR",
+            "USD"});
+            this.cbCurrency.Location = new System.Drawing.Point(444, 10);
+            this.cbCurrency.Name = "cbCurrency";
+            this.cbCurrency.Size = new System.Drawing.Size(121, 24);
+            this.cbCurrency.TabIndex = 4;
+            this.cbCurrency.Text = "EUR";
+            this.cbCurrency.SelectedIndexChanged += new System.EventHandler(this.cbCurrency_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1000, 451);
+            this.Controls.Add(this.cbCurrency);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.chartRateData);
             this.Controls.Add(this.dgwRates);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgwRates)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -59,6 +117,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgwRates;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRateData;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.ComboBox cbCurrency;
     }
 }
 
--- MnbCurrencyReader/MnbCurrencyReader/Form1.cs
+++ MnbCurrencyReader/MnbCurrencyReader/Form1.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml;
 
 namespace MnbCurrencyReader
 {
@@ -19,26 +21,80 @@ namespace MnbCurrencyReader
         {
             InitializeComponent();
             dgwRates.DataSource = Rates;
-            ArfolyamLekerdezes();
+            RefreshData();
         }
 
-        private void ArfolyamLekerdezes()
+        private void RefreshData()
+        {
+            Rates.Clear();
+            var result = ArfolyamLekerdezes();
+            ArfolyamFeldolgozas(result);
+            ChartFeltoltes();
+        }
+
+        private void ChartFeltoltes()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void ArfolyamFeldolgozas(string result)
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText.Replace(',','.')); //Tizedes vesszot pontra cserelem
+                if (unit != 0) rate.Value = value / unit;
+            }
+        }
+
+        private string ArfolyamLekerdezes()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = cbCurrency.SelectedItem.ToString(),
+                startDate = dtpStart.Value.ToString("yyyy-MM-dd"),
+                endDate = dtpEnd.Value.ToString("yyyy-MM-dd")
             };
             var response= mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
             Console.WriteLine(result);
+            return result;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void dtpStart_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void cbCurrency_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
