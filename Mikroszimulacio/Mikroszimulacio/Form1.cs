b916a6de0fdf9e00997610420603ffe3792fed7e
adriennzeman <adrienn.zeman@stud.uni-corvinus.hu>
2022-01-09 18:13:40 +0100

    Adatok betoltese

--- Mikroszimulacio/Mikroszimulacio/Form1.cs
+++ Mikroszimulacio/Mikroszimulacio/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Mikroszimulacio.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,79 @@ namespace Mikroszimulacio
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbabilities> BirthProbabilities = new List<BirthProbabilities>();
+        List<DeathProbabilities> DeathProbabilities = new List<DeathProbabilities>();
+
         public Form1()
         {
             InitializeComponent();
+            GetPopulation("C:/Temp/nép-teszt.csv");
+            GetBirthProbabilities("C:/Temp/születés.csv");
+            GetDeathProbabilities("C:/Temp/halál.csv");
+        }
+
+        public List<Person> GetPopulation(string csvpath)
+        {
+            List <Person> population = new List<Person>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+            return population;
+        }
+
+        public List<BirthProbabilities> GetBirthProbabilities(string csvpath)
+        {
+            List<BirthProbabilities > birthProbabilities = new List<BirthProbabilities>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    birthProbabilities.Add(new BirthProbabilities()
+                    {
+                        Age = int.Parse(line[0]),
+                        NbrOfChildren = int.Parse(line[1]),
+                        P = double.Parse(line[2].Replace(',','.'))
+
+                    });
+                }
+            }
+            return birthProbabilities;
+        }
+
+        public List<DeathProbabilities> GetDeathProbabilities(string csvpath)
+        {
+            List<DeathProbabilities> deathProbabilities = new List<DeathProbabilities>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    deathProbabilities.Add(new DeathProbabilities()
+                    {
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
+                        Age = int.Parse(line[1]),
+                        P = double.Parse(line[2].Replace(',', '.'))
+
+                    });
+                }
+            }
+            return deathProbabilities;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
