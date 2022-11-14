d7d4e66b8ba4139c036f5b3a29c0b9ddf8b78953
adriennzeman <adrienn.zeman@stud.uni-corvinus.hu>
2022-11-14 13:13:28 +0100

    3. feladat

--- Linq_feladat/Linq_feladat/Form1.cs
+++ Linq_feladat/Linq_feladat/Form1.cs
@@ -14,11 +14,30 @@ namespace Linq_feladat
     public partial class Form1 : Form
     {
         private List<Country> countries = new List<Country>();
+        private List<Ramen> ramens = new List<Ramen>();
+
 
         public Form1()
         {
             InitializeComponent();
             LoadData("ramen.csv");
+        }
+
+        private Country AddCountry(string countryName)
+        {
+            var currentCountry = (from c in countries
+                                  where c.Name.Equals(countryName)
+                                  select c).FirstOrDefault();
+            if (currentCountry == null)
+            {
+                currentCountry = new Country()
+                {
+                    ID = countries.Count + 1,
+                    Name = countryName
+                };
+                countries.Add(currentCountry);
+            }
+            return currentCountry;
         }
 
         private void LoadData(string fileName)
@@ -31,18 +50,17 @@ namespace Linq_feladat
                     var line = sr.ReadLine().Split(';');
 
                     var countryName = line[2];
-                    var currentCountry = (from c in countries
-                                          where c.Name.Equals(countryName)
-                                          select c).FirstOrDefault();
-                    if (currentCountry == null)
+                    var country = AddCountry(countryName);
+                    var ramen = new Ramen()
                     {
-                        currentCountry = new Country()
-                        {
-                            ID = countries.Count + 1,
-                            Name = countryName
-                        };
-                        countries.Add(currentCountry);
-                    }
+                        ID = ramens.Count + 1,
+                        Brand = line[0],
+                        Name = line[1],
+                        CountryFK = country.ID,
+                        Country = country,
+                        Rating = Convert.ToDouble(line[3])
+                    };
+                    ramens.Add(ramen);
                 }
             }
         }
